532a4329db2d7d76aa494c497e09a5e403ddf2fc
keigo-watanabe <k.w.99jun21@icloud.com>
2021-05-14 16:35:59 +0900

    会員登録・ログイン・パスワード再発行ページのカスタマイズ

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,10 +1,10 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <h1 class="register-title">パスワード再発行</h1>
         </x-slot>
 
-        <div class="mb-4 text-sm text-gray-600">
+        <div class="mb-4 text-sm text-gray-600 forgot-password-sentence">
             {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
         </div>
 
@@ -19,13 +19,13 @@
         <form method="POST" action="{{ route('password.email') }}">
             @csrf
 
-            <div class="block">
+            <div class="block form-input">
                 <x-jet-label for="email" value="{{ __('Email') }}" />
                 <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <x-jet-button>
+            <div class="form-register-btn">
+                <x-jet-button class="form-register">
                     {{ __('Email Password Reset Link') }}
                 </x-jet-button>
             </div>
