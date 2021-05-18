79e29b4d3b82088d48abb0059b1350f545bd1d63
keigo-watanabe <k.w.99jun21@icloud.com>
2021-05-18 21:29:15 +0900

    goalテーブルmigration

---
+++ app/Models/Goal.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Goal extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    /*
+    主キーの変更
+    */
+    protected $primaryKey = 'goal_id';
+}
