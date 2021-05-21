00ececc2c9f0c9a69cb7f7bc0aa185c89a8721ac
keigo-watanabe <k.w.99jun21@icloud.com>
2021-05-21 13:20:44 +0900

    task migrate

---
+++ app/Models/Task.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Task extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+    
+    /*
+    主キーの変更
+    */
+    protected $primaryKey = 'task_id';
+}
