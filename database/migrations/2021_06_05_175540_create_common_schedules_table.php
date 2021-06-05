a542da3da443c8c2183c52d7bd73bae78b5fcbfd
keigo-watanabe <k.w.99jun21@icloud.com>
2021-06-05 18:01:21 +0900

    common_schedule migrate

---
+++ database/migrations/2021_06_05_175540_create_common_schedules_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommonSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('common_schedules', function (Blueprint $table) {
+            $table->id('common_schedule_id');
+            $table->string('title');
+            $table->string('common_color');
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->unique('title');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('common_schedules');
+    }
+}
