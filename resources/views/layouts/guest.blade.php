2efc759189a5e03a960d50599a80e0c99f71d077
keigo-watanabe <k.w.99jun21@icloud.com>
2021-06-12 13:56:00 +0900

    会員登録・ログインページ編集

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -23,29 +23,50 @@
     </head>
     <body>
       <!-- ヘッダー -->
-      <header class="lp-header lp-header-bg lp-header-register">
-        <div class="header-logo">
-          <a href="/"><span id="logo" class="logo">Goal<span class="logo-achivement">Achivement.</span></span></a>
-        </div>
-      </header>
+      <header id="lp-header" class="lp-header">
+        <div class="header-inner">
+          <div class="header-logo">
+            <span class="goalachivement">Goal<span class="logo-achivement">Achivement.</span></span>
+          </div>
 
-        <div class="font-sans text-gray-900 antialiased">
-          <div class="register-container">
-            <div class="register-container-bg">
-              {{ $slot }}
+          <nav id="header-nav" class="header-nav">
+            <ul>
+              <li class="nav-li"><a href="/">ホーム</a></li>
+              <li class="nav-li"><a href="#app-management">アプリの使い方</a></li>
+              <li class="nav-li"><a href="#app-feature">機能</a></li>
+              <li class="nav-li"><a href="/lp/contact">お問い合わせ</a></li>
+              <li class="header-register"><a href="{{ route('register') }}">会員登録</a></li>
+              <li class="header-login"><a href="{{ route('login') }}">ログイン</a></li>
+            </ul>
+          </nav>
+
+          <div id="header-nav-btn" class="header-nav-btn">
+            <div class="bars">
+              <span class="bar bar1"></span>
+              <span class="bar bar2"></span>
+              <span class="bar bar3"></span>
             </div>
           </div>
         </div>
+      </header>
 
-        <!-- フッター -->
-        <footer class="lp-footer">
-          <div class="footer-logo">
-            <a href=""><span class="logo">Goal<span class="logo-achivement">Achivement.</span></span></a>
-          </div>
+      <main>
+        <!-- コンポーネント -->
+        {{ $slot }}
+      </main>
 
-          <div class="copyright">
-            <span>©️Goal Achievement 2021</span>
-          </div>
-        </footer>
+      <!-- フッター -->
+      <footer class="lp-footer">
+        <div class="footer-logo">
+          <span class="goalachivement">Goal<span class="logo-achivement">Achivement.</span></span>
+        </div>
+
+        <div class="copyright">
+          <span>©️ Copyright GoalAchivement All right reserved.</span>
+        </div>
+      </footer>
+
+      <script src="/js/jquery-3.5.1.min.js"></script>
+      <script src="{{ asset('js/script.js') }}"></script>
     </body>
 </html>
